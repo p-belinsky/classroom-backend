bc2bdc40abaa23f011637ed2bfa68b35d8790c12
Phil Belinsky <philbelinsky@yahoo.com>
2026-01-16 13:07:40 -0500

    implement suggestions from code rabbit

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import cors from 'cors';
 const app = express();
 const port = 8000;
 
+const frontendUrl = process.env.FRONTEND_URL;
+if(!frontendUrl) throw new Error(
+  'FRONTEND_URL is not set in .env file'
+)
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: frontendUrl,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
